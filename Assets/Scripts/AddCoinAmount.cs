7c3c5457ab11ce211c91b8f6f43e84bdb8ba2a0e
Dankgard <dangar18@ucm.es>
2020-12-29 13:30:48 +0100

    Evento de coger moneda

---
+++ Assets/Scripts/AddCoinAmount.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCoinAmount : MonoBehaviour
+{
+    BoxCollider collider;
+    float coinAmount;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            collider.enabled = false;
+            GetComponent<FMODUnity.StudioEventEmitter>().Play();
+            FMODUnity.RuntimeManager.StudioSystem.getParameterByName("CoinAmount", out coinAmount);
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("CoinAmount", coinAmount + 1);
+        }
+    }
+}
