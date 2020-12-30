7757813e5929a1389d8e70f244bfb2c30d6b00c2
Dankgard <dangar18@ucm.es>
2020-12-30 13:12:12 +0100

    Evento de dispajaro del pajaro

---
+++ Assets/Scripts/BirdShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdShoot : MonoBehaviour
+{
+    Transform playerTransform;
+    Transform birdTransform;
+    float distance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        birdTransform = GameObject.FindWithTag("Bird").GetComponent<Transform>();
+        distance = Vector3.Distance(playerTransform.position, birdTransform.position);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("DistanceToBird", Mathf.Min(30, distance));
+        GetComponent<FMODUnity.StudioEventEmitter>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
