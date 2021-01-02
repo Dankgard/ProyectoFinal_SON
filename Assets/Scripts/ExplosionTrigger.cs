1a04ff8a3bfd3dd0d0adb67b5d3cf86b11cf552f
Dankgard <dangar18@ucm.es>
2021-01-02 13:07:15 +0100

    Evento explosion valla

---
+++ Assets/Scripts/ExplosionTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTrigger : MonoBehaviour
+{
+    bool triggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !triggered)
+        {
+            triggered = true;
+            GameObject.FindWithTag("Explosion").GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        }
+    }
+}
